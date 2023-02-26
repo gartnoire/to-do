68c8178df63f76dd6c682fce9a276a8b25b8a801
*** <108582867+gartnoire@users.noreply.github.com>
2023-02-26 12:41:16 +0400

    Added: header for a block, plugging in styles and page styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./views/App";
 
+import "./views/styles/common.css";
+import "./views/styles/reset.css";
+import "./views/styles/var.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,5 +1,13 @@
 import { ReactElement } from "react";
 
+import styles from "./index.module.css";
+
 export function App(): ReactElement {
-  return <article></article>;
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>To-Do List</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
+  );
 }
